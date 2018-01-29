68318882bfda35e820180fb319815d7093a3b33b
Leandro Späth <leandro.spaeth@gmail.com>
2018-01-29 14:28:35 +0100

    add properties file

--- 02_Komponenten/src/Configuration.java
+++ 02_Komponenten/src/Configuration.java
@@ -1,3 +1,6 @@
+import java.io.FileInputStream;
+import java.util.Properties;
+
 public enum Configuration {
     instantce;
 
@@ -8,5 +11,30 @@ public enum Configuration {
     public String jarPath = "exchange_component_" + sortingAlgorithm + fileSeparator + "jar" + fileSeparator + "sorter.jar";
     public String fullJarPath = userDirectory + jarPath;
     public String className = "Sorter";
+
+    public String propFile = "sorter.props";
+
+    public SorterType getSorterType() {
+        try {
+            Properties props = new Properties();
+            FileInputStream fis = new FileInputStream(userDirectory + fileSeparator + propFile);
+            props.load(fis);
+            fis.close();
+            String selectedProp = props.getProperty("sorterType");
+            switch (selectedProp) {
+                case "intro":
+                    return SorterType.intro;
+                case "shaker":
+                    return SorterType.shaker;
+                default:
+                    return null;
+            }
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return null;
+    }
+
 
 }
---
+++ 02_Komponenten/src/SorterType.java
@@ -0,0 +1,3 @@
+public enum SorterType {
+    intro,shaker
+}
