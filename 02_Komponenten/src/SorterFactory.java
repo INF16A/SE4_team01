89353bb90640d21db9006a6e9c4d8d73ce9fc7b5
Leandro Späth <leandro.spaeth@gmail.com>
2018-01-29 16:17:29 +0100

    fix component loading

--- 02_Komponenten/src/SorterFactory.java
+++ 02_Komponenten/src/SorterFactory.java
@@ -6,7 +6,7 @@ public class SorterFactory {
     public static Object create() {
         Object sortPort = null;
         try {
-            URL[] urls = {new File(Configuration.instance.jarPath).toURI().toURL()};
+            URL[] urls = {new File(Configuration.instance.getJarPath()).toURI().toURL()};
             URLClassLoader urlLoader = new URLClassLoader(urls, SorterFactory.class.getClassLoader());
             Class sorterClass = Class.forName(Configuration.instance.className, true, urlLoader);
             Object sorterInstance = sorterClass.getMethod("getInstance", new Class[0]).invoke(null, new Object[0]);
