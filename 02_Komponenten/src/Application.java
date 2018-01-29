c4bf1c60c62569025d39c25371a89ac556e0bdf9
armbeck16 <arm.beck.16@lehre.mosbach.dhbw.de>
2018-01-29 16:01:48 +0100

    Merge branch 'master' of C:\Users\armbe\IdeaProjects\SE4_team01 with conflicts.

--- 02_Komponenten/src/Application.java
+++ 02_Komponenten/src/Application.java
@@ -1,37 +1,40 @@
-import sun.security.krb5.Config;
 
 import java.lang.reflect.Method;
 import java.util.Scanner;
 
 public class Application {
+    private Application() {
+        loadComponent(Configuration.instance.getSorterType());
+    }
+
     private Class _class;
     private Object _classInstance;
     private Object port;
 
     private void showComponents() {
-
+        SorterType[] sorterTypes = SorterType.values();
+        for (SorterType sorterType :
+                sorterTypes) {
+            System.out.println(sorterType.name());
+        }
     }
 
     private void showCurrentComponent() {
-        try {
-            Method getName = port.getClass().getMethod("getName");
-            System.out.println("current component: " + getName.invoke(port));
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        SorterType[] sorterTypes = SorterType.values();
+        System.out.println("current component " + sorterTypes[-1 + Integer.parseInt(Configuration.instance.sortingAlgorithm)]);
     }
 
     private void parseInputSelection(String input) {
-        int parsedInput = -1 + Integer.parseInt(input);
+        String parsedInput = input.toLowerCase().trim();
         SorterType[] sorterTypes = SorterType.values();
         for (int i = 0; i < sorterTypes.length; i++) {
-            if (parsedInput == i) {
+            if (parsedInput.equals(sorterTypes[i].name())) {
                 System.out.println(sorterTypes[i].name() + " sorter selected");
                 loadComponent(sorterTypes[i]);
-                break;
+                return;
             }
         }
+        System.out.println("selection not changed '" + parsedInput + "'");
     }
 
     private void loadComponent(SorterType num) {
@@ -62,7 +65,7 @@ public class Application {
 
     public static void main(String... args) {
         Application app = new Application();
-
+        System.out.println("Enter a command pls");
         boolean running = true;
         while (running) {
             Scanner scanner = new Scanner(System.in);
@@ -80,7 +83,7 @@ public class Application {
                 app.parseInputSelection(parts[3]);
             } else if (command.startsWith("execute ")) {
                 String parts[] = command.split(" ");
-                String arr[] = parts[2].split(",");
+                String arr[] = parts[1].split(",");
                 app.execute(arr);
             } else {
                 System.out.println("Unknown command.\nThe following commands are available:\nshow components, show current component, set current component <component>, execute <unsorted comma seperated list>");
