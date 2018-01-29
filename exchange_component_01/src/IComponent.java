4dca4b9aa8b45b97a2b01a1ec878f25a6aadc1d2
Leandro Späth <leandro.spaeth@gmail.com>
2018-01-29 11:08:24 +0100

    copied sort algorithms

--- exchange_component_01/src/IComponent.java
+++ exchange_component_01/src/IComponent.java
@@ -1,2 +1,3 @@
 public interface IComponent {
+    static void sort(int[] a) {};
 }
--- exchange_component_02/src/Component.java
+++ exchange_component_02/src/Component.java
@@ -1,3 +1,31 @@
-public class Component {
+//source: http://www.programming-algorithms.net/article/40270/Shaker-sort
 
+public class Component implements IComponent{
+    /**
+     * Shaker sort (bidirectional bubble sort)
+     * Orders in descending order
+     * @param array array to be sorted
+     */
+    public static void sort(int[] array) {
+        for (int i = 0; i < array.length/2; i++) {
+            boolean swapped = false;
+            for (int j = i; j < array.length - i - 1; j++) {
+                if (array[j] < array[j+1]) {
+                    int tmp = array[j];
+                    array[j] = array[j+1];
+                    array[j+1] = tmp;
+                    swapped = true;
+                }
+            }
+            for (int j = array.length - 2 - i; j > i; j--) {
+                if (array[j] > array[j-1]) {
+                    int tmp = array[j];
+                    array[j] = array[j-1];
+                    array[j-1] = tmp;
+                    swapped = true;
+                }
+            }
+            if(!swapped) break;
+        }
+    }
 }
