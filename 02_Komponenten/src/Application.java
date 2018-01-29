ab4fa06504b2f4aa0f5e6b29afc9a71547c56003
armbeck16 <arm.beck.16@lehre.mosbach.dhbw.de>
2018-01-29 14:55:53 +0100

    various commands implemented a bit more

--- 02_Komponenten/src/Application.java
+++ 02_Komponenten/src/Application.java
@@ -1,21 +1,58 @@
+import sun.security.krb5.Config;
+
+import java.lang.reflect.Method;
 import java.util.Scanner;
 
 public class Application {
+    private Class _class;
+    private Object _classInstance;
+    private Object port;
 
     private void showComponents() {
 
     }
 
     private void showCurrentComponent() {
-
+        SorterType[] sorterTypes = SorterType.values();
+        System.out.println("current component" + sorterTypes[Integer.parseInt(Configuration.instantce.sortingAlgorithm)]);
     }
 
-    private void loadComponent(String num) {
-
+    private void parseInputSelection(String input) {
+        int parsedInput = -1 + Integer.parseInt(input);
+        SorterType[] sorterTypes = SorterType.values();
+        for (int i = 0; i < sorterTypes.length; i++) {
+            if (parsedInput == i) {
+                System.out.println(sorterTypes[i].name() + " sorter selected");
+                loadComponent(sorterTypes[i]);
+                break;
+            }
+        }
     }
 
-    private void execute(String arr[]) {
+    private void loadComponent(SorterType num) {
+        Configuration.instantce.sortingAlgorithm = "0" + Integer.toString(num.ordinal() + 1);
+        port = SorterFactory.create();
+    }
 
+    private int[] execute(String arr[]) {
+
+        try {
+            int[] values = new int[arr.length];
+            for (int i = 0; i < arr.length; i++) {
+                values[i] = Integer.parseInt(arr[i]);
+            }
+            Method sortMethod = port.getClass().getMethod("sort", int[].class);
+            return (int[]) sortMethod.invoke(port, arr);
+
+        } catch (NumberFormatException e) {
+            System.out.println("Invalid number input");
+        } catch (NoSuchMethodException e) {
+            System.out.println("Method 'sort' not found.");
+        } catch (Exception e) {
+            System.out.println("unexpected exception happened.");
+            e.printStackTrace();
+        }
+        return new int[0];
     }
 
     public static void main(String... args) {
@@ -27,16 +64,16 @@ public class Application {
 
             String command = scanner.nextLine();
 
-            if(command.equals("exit")) {
+            if (command.equals("exit")) {
                 running = false;
-            } else if(command.equals("show components")) {
+            } else if (command.equals("show components")) {
                 app.showComponents();
-            } else if(command.equals("show current component")) {
+            } else if (command.equals("show current component")) {
                 app.showCurrentComponent();
-            } else if(command.startsWith("set current component ")) {
+            } else if (command.startsWith("set current component ")) {
                 String[] parts = command.split(" ");
-                app.loadComponent(parts[3]);
-            } else if(command.startsWith("execute ")) {
+                app.parseInputSelection(parts[3]);
+            } else if (command.startsWith("execute ")) {
                 String parts[] = command.split(" ");
                 String arr[] = parts[2].split(",");
                 app.execute(arr);
