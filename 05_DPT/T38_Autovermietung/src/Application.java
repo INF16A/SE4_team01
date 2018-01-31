ffd22c670d9a3e991a3b42cafde1ea1211a43225
Leandro Späth <leandro.spaeth@gmail.com>
2018-01-31 16:14:28 +0100

    intial commit

---
+++ 05_DPT/T38_Autovermietung/src/Application.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Application {
+    public static void main(String... args) {
+        System.out.println("Commands: RENT <type>, RETURN <id>");
+        Scanner scanner = new Scanner(System.in);
+
+    }
+}
