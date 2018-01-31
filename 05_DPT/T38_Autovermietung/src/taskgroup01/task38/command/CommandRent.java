8ed1b856826d0b425f5ca62cb7e32a822e0eb9aa
Leandro Späth <leandro.spaeth@gmail.com>
2018-01-31 18:02:36 +0100

    add command stub

---
+++ 05_DPT/T38_Autovermietung/src/taskgroup01/task38/command/CommandRent.java
@@ -0,0 +1,15 @@
+package taskgroup01.task38.command;
+
+import taskgroup01.task38.Vehicle;
+
+public class CommandRent {
+    private Vehicle vehicle;
+
+    public CommandRent(Vehicle vehicle) {
+        this.vehicle = vehicle;
+    }
+
+    public void execute() {
+
+    }
+}
---
+++ 05_DPT/T38_Autovermietung/src/taskgroup01/task38/command/CommandReturn.java
@@ -0,0 +1,15 @@
+package taskgroup01.task38.command;
+
+import taskgroup01.task38.Vehicle;
+
+public class CommandReturn {
+    private Vehicle vehicle;
+
+    public CommandReturn(Vehicle vehicle) {
+        this.vehicle = vehicle;
+    }
+
+    public void execute() {
+
+    }
+}
