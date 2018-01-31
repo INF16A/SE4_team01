699dc287bcf9b8a6a984ba0dde525d067f3571cc
armbeck16 <arm.beck.16@lehre.mosbach.dhbw.de>
2018-01-31 15:17:22 +0100

    Airplane implementation finished a bit more

--- 05_DPT/T47_Flugsicherung/src/Airplane.java
+++ 05_DPT/T47_Flugsicherung/src/Airplane.java
@@ -1,14 +1,22 @@
 import java.util.ArrayList;
 import java.util.List;
 
-public class Airplane {
-    private int height;
-    private int[] location;
-    private int speed;
+public class Airplane implements ITimeControlled {
+    private static final float changeHeightChance = 0.2f;
+    private static final int minHeight = 9500;
+    private static final int maxHeight = 11250;
+    private static final int maxEmergencyHeight = 9000;
+    private static final int minEmergencyHeight = 11500;
+    private int height = 10000;
+    private int position;
+    private int speed = 20;
+    private String name;
+
     private IFlightControl flightControl;
     private List<IPositionListener> listeners = new ArrayList<>();
 
-    public Airplane(IFlightControl fc) {
+    public Airplane(IFlightControl fc, String name) {
+        this.name = name;
         this.flightControl = fc;
         this.flightControl.registerAirplane(this);
     }
@@ -20,4 +28,43 @@ public class Airplane {
     public void removeListener(IPositionListener positionListener) {
         listeners.remove(positionListener);
     }
+
+    @Override
+    public void Cycle() {
+        position += speed;
+        //change height random based
+        double chance = Application.GetRandomChance();
+        if (chance < changeHeightChance) {
+            if (chance < changeHeightChance / 2) {
+                height += 250;
+            } else if (chance >= changeHeightChance / 2) {
+                height -= 250;
+            }
+        }
+
+        // correct flight height
+        if (height > maxHeight) {
+            height = maxHeight;
+        }
+        if (height < minHeight) {
+            height = minHeight;
+        }
+
+        System.out.print("[" + name + "] now flying at position " + position + ", altitude " + height);
+        notifyListeners();
+    }
+
+    public void emergencyClimb() {
+        System.out.print("[" + name + "] Emergency climb from " + height + " to");
+        height += 500;
+        System.out.println(height);
+    }
+
+    private void notifyListeners() {
+        for (int i = 0; i < listeners.size(); i++) {
+            listeners.get(i).positionChanged(this, position, height);
+        }
+    }
+
+
 }
--- 05_DPT/T47_Flugsicherung/src/Application.java
+++ 05_DPT/T47_Flugsicherung/src/Application.java
@@ -1,13 +1,18 @@
-import java.lang.reflect.Array;
+import java.util.Random;
 
 public class Application {
     public static void main(String args) {
         IFlightControl flightControl = new FlightControl();
-        Airplane F1 = new Airplane(flightControl);
-        Airplane F2 = new Airplane(flightControl);
+        Airplane F1 = new Airplane(flightControl,"F1");
+        Airplane F2 = new Airplane(flightControl,"F2");
         Airport A = new Airport(flightControl, 0, 0);
         Airport B = new Airport(flightControl, 1000, 0);
 
+    }
 
+    private static Random random = new Random();
+
+    public static float GetRandomChance() {
+        return random.nextFloat();
     }
 }
