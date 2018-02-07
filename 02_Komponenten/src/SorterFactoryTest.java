75dcf6eb4b082c3cf4d1e605d76dc387145b79fb
armbeck16 <arm.beck.16@lehre.mosbach.dhbw.de>
2018-02-07 23:03:18 +0100

    tests created for SorterFactory

--- 02_Komponenten/src/SorterFactoryTest.java
+++ 02_Komponenten/src/SorterFactoryTest.java
@@ -1,16 +1,19 @@
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class SorterFactoryTest {
+
+
     @Test
     public void testFactory() {
-        Configuration.instance.getSorterType();
+        Configuration.instance.className = "Sorter";
         Assert.assertNotNull(SorterFactory.create());
     }
 
     @Test
-    public  void testFactoryFail(){
-        Configuration.instance.className=null;
+    public void testFactoryFail() {
+        Configuration.instance.className = null;
         Assert.assertNull(SorterFactory.create());
     }
 }
