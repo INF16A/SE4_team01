b99691dbbc2a13006256167a9a88221544f3f45a
Leandro Späth <leandro.spaeth@gmail.com>
2018-02-10 00:01:04 +0100

    clean up getters

--- 05_DPT/T38_Autovermietung/src/taskgroup01/task38/Repository.java
+++ 05_DPT/T38_Autovermietung/src/taskgroup01/task38/Repository.java
@@ -2,116 +2,87 @@ package taskgroup01.task38;
 
 import taskgroup01.task38.vehicleType.*;
 
-public class Repository {
-    private V1Storage v1Storage;
-    private V2Storage v2Storage;
-    private V3Storage v3Storage;
-    private V4Storage v4Storage;
-    private V5Storage v5Storage;
-    private IIterator v1Iterator;
-    private IIterator v2Iterator;
-    private IIterator v3Iterator;
-    private IIterator v4Iterator;
-    private IIterator v5Iterator;
+import java.util.ArrayList;
+import java.util.List;
 
-    Reservation reservation;
+public class Repository {
+    private List<VehicleStorage> storages = new ArrayList<>();
+    private List<IIterator> iterators = new ArrayList<>();
+
+    private Reservation reservation;
 
     public Repository() {
         reservation = new Reservation();
 
         // there must be a better way for this, but I can't think how currently. TODO: make better
-        v1Storage = new V1Storage(3);
-        v1Iterator = v1Storage.iterator();
+        V1Storage v1Storage = new V1Storage(3);
+        IIterator v1Iterator = v1Storage.iterator();
         v1Storage.appendVehicle(new V1("BS-AF1001"));
         v1Storage.appendVehicle(new V1("BS-AF1002"));
         v1Storage.appendVehicle(new V1("BS-AF1003"));
+        storages.add(v1Storage);
+        iterators.add(v1Iterator);
 
-        v2Storage = new V2Storage(3);
-        v2Iterator = v2Storage.iterator();
+        V2Storage v2Storage = new V2Storage(3);
+        IIterator v2Iterator = v2Storage.iterator();
         v2Storage.appendVehicle(new V2("BS-AF2001"));
         v2Storage.appendVehicle(new V2("BS-AF2002"));
         v2Storage.appendVehicle(new V2("BS-AF2003"));
+        storages.add(v2Storage);
+        iterators.add(v2Iterator);
 
-        v3Storage = new V3Storage(3);
-        v3Iterator = v3Storage.iterator();
+        V3Storage v3Storage = new V3Storage(3);
+        IIterator v3Iterator = v3Storage.iterator();
         v3Storage.appendVehicle(new V3("BS-AF3001"));
         v3Storage.appendVehicle(new V3("BS-AF3002"));
         v3Storage.appendVehicle(new V3("BS-AF3003"));
+        storages.add(v3Storage);
+        iterators.add(v3Iterator);
 
-        v4Storage = new V4Storage(3);
-        v4Iterator = v4Storage.iterator();
+        V4Storage v4Storage = new V4Storage(3);
+        IIterator v4Iterator = v4Storage.iterator();
         v4Storage.appendVehicle(new V4("BS-AF4001"));
         v4Storage.appendVehicle(new V4("BS-AF4002"));
         v4Storage.appendVehicle(new V4("BS-AF4003"));
+        storages.add(v4Storage);
+        iterators.add(v4Iterator);
 
-        v5Storage = new V5Storage(3);
-        v5Iterator = v5Storage.iterator();
+        V5Storage v5Storage = new V5Storage(3);
+        IIterator v5Iterator = v5Storage.iterator();
         v5Storage.appendVehicle(new V5("BS-AF5001"));
         v5Storage.appendVehicle(new V5("BS-AF5002"));
         v5Storage.appendVehicle(new V5("BS-AF5003"));
+        storages.add(v5Storage);
+        iterators.add(v5Iterator);
     }
 
-    // there must be a better way for this, but I can't think how currently. TODO: make better
     private Vehicle getVehicleByLicensePlate(String plate) {
-        Vehicle v = null;
-        v = v1Storage.getVehicleByPlate(plate);
-        if (v != null) return v;
-        v = v2Storage.getVehicleByPlate(plate);
-        if (v != null) return v;
-        v = v3Storage.getVehicleByPlate(plate);
-        if (v != null) return v;
-        v = v4Storage.getVehicleByPlate(plate);
-        if (v != null) return v;
-        v = v5Storage.getVehicleByPlate(plate);
-        return v;
+        for (VehicleStorage storage : storages) {
+            Vehicle v = storage.getVehicleByPlate(plate);
+            if(v != null) return v;
+        }
+        return null;
     }
 
     private VehicleStorage getStorageForVehicle(Vehicle v) {
+
         String className = v.getClass().toString();
-        if(className.contains("V1"))
-            return v1Storage;
-        else if(className.contains("V2"))
-            return v2Storage;
-        else if(className.contains("V3"))
-            return v3Storage;
-        else if(className.contains("V4"))
-            return v4Storage;
-        else if(className.contains("V5"))
-            return v5Storage;
-        else
-            return null;
+        String[] classNameChecks = {"V1", "V2", "V3", "V4", "V5"};
+        int i = 0;
+        for (String classNameCheck : classNameChecks) {
+            if(className.contains(classNameCheck))
+                return storages.get(i);
+            i++;
+        }
+        return null;
     }
 
     private VehicleStorage getStorageForType(int vehicleType) {
-        switch (vehicleType) {
-            case 1:
-                return v1Storage;
-            case 2:
-                return v2Storage;
-            case 3:
-                return v3Storage;
-            case 4:
-                return v4Storage;
-            case 5:
-                return v5Storage;
-        }
-        return null;
+        return storages.get(vehicleType - 1);
     }
 
     private IIterator getIteratorForType(int vehicleType) {
-        switch (vehicleType) {
-            case 1:
-                return v1Iterator;
-            case 2:
-                return v2Iterator;
-            case 3:
-                return v3Iterator;
-            case 4:
-                return v4Iterator;
-            case 5:
-                return v5Iterator;
-        }
-        return null;
+        return iterators.get(vehicleType - 1);
     }
 
 
@@ -134,20 +105,19 @@ public class Repository {
         Vehicle v = getVehicleByLicensePlate(plate);
         if (v != null) {
             VehicleStorage vs = getStorageForVehicle(v);
-            if(v.getRentedBy() != null) {
-                if(v.getRentedBy().equals(customer)) {
+            if (v.getRentedBy() != null) {
+                if (v.getRentedBy().equals(customer)) {
                     vs.setTenant(v, null);
                     System.out.println("Successfully returned vehicle");
                     reservation.sendNotification(v.getType());
-                }
-                else {
+                } else {
                     System.out.println("ERROR: You are unauthorized to return the vehicle rented by " + v.getRentedBy().getName());
                 }
             } else {
                 System.out.println("ERROR: Can't return car that is not rented.");
             }
         } else {
-            System.out.println("ERROR: No vehicle with plate " + plate + " exists");
+            System.out.println("ERROR: No vehicle with plate \"" + plate + "\" exists");
         }
     }
 
