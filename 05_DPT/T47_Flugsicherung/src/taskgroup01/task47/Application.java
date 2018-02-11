f7e970b7d1732d2c01ccc164a2ba1ee6e358fd3c
armbeck16 <arm.beck.16@lehre.mosbach.dhbw.de>
2018-02-11 17:35:09 +0100

    timer created, tests adjusted

--- 05_DPT/T47_Flugsicherung/src/taskgroup01/task47/Application.java
+++ 05_DPT/T47_Flugsicherung/src/taskgroup01/task47/Application.java
@@ -1,8 +1,13 @@
 package taskgroup01.task47;
 
 import java.util.Random;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class Application {
+
+    public static int timerInterval = 50;
+
     public static void main(String... args) {
         IFlightControl flightControl = new FlightControl();
         Airport A = new Airport(0);
@@ -11,9 +16,25 @@ public class Application {
         Airplane F1 = new Airplane(flightControl, "F1", 20, A);
         Airplane F2 = new Airplane(flightControl, "F2", -20, B);
 
-        for (int i = 0; i < 50; i++) {
-            TimeControl.timeControl.Cycle();
-        }
+        startTimer();
+    }
+
+    private static void startTimer() {
+        Timer timer = new Timer();
+        TimerTask timerTask = new TimerTask() {
+            private int counter = 0;
+
+            @Override
+            public void run() {
+                TimeControl.timeControl.Cycle();
+                counter++;
+                if (counter == 50) {
+                    this.cancel();
+                    timer.cancel();
+                }
+            }
+        };
+        timer.schedule(timerTask, timerInterval, timerInterval);
     }
 
     private static Random random = new Random();
--- 05_DPT/T47_Flugsicherung/test/taskgroup01/task47/TestApplicationMain.java
+++ 05_DPT/T47_Flugsicherung/test/taskgroup01/task47/TestApplicationMain.java
@@ -1,16 +1,30 @@
 package taskgroup01.task47;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 public class TestApplicationMain {
+
+    @Before
+    public void setUp() {
+        Application.timerInterval = 10;
+    }
+
     @Test
-    public void TestMain(){
+    public void TestMain() {
         Application.main(null);
     }
+
     @Test
-    public void TestMainConsistency(){
+    public void TestMainConsistency() {
         for (int i = 0; i < 100; i++) {
             Application.main(null);
         }
+    }
+
+    @After
+    public void tearDown() {
+        Application.timerInterval = 1000;
     }
 }
