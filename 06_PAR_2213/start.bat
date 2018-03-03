9c98083362a074322ee4ac26ff89cb840d61c260
armbeck16 <arm.beck.16@lehre.mosbach.dhbw.de>
2018-03-03 17:21:45 +0100

    graphical output added

--- 06_PAR_2213/start.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-java -server -Xms6G -Xmx6G -XX:MaxDirectMemorySize=1024M -XX:NewSize=1G -XX:MaxNewSize=1G -XX:+UseParNewGC -XX:MaxTenuringThreshold=2 -XX:SurvivorRatio=8 -XX:+UnlockDiagnosticVMOptions -XX:ParGCCardsPerStrideChunk=32768 -classpath out\production\06_PAR_2213 main.Application
-PAUSE
--- 09_KPXLT/src/main/Application.java
+++ 09_KPXLT/src/main/Application.java
@@ -9,34 +9,92 @@ import javafx.scene.image.WritableImage;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class Application extends javafx.application.Application {
 
-    public static void main(String[] args) throws  Exception{
-        Simulation sim = new Simulation(100, 0);
-        SimulationExecutor executor = new SimulationExecutorMultiThreaded(sim);
+    public static void main(String[] args) throws Exception {
+     /*   Simulation simulation = new Simulation(100, 0.0f);
+        SimulationExecutor executor = new SimulationExecutorMultiThreaded(simulation);
         executor.AddListener(new ISimulationObserver() {
             @Override
             public void stepFinished() {
-                System.out.println("next step!");
+                System.out.println(".");
             }
         });
-        executor.start();
+        executor.start();*/
         launch(args);
-        executor.stop();
+        // executor.stop();
+
     }
+
+    private Simulation simulation;
+    private SimulationExecutor executor;
+    private ExecutorService drawExecutor;
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+        this.simulation = new Simulation(10, 0.1f);
+        this.executor = new SimulationExecutorSingleThreaded(simulation);
+        this.drawExecutor = Executors.newSingleThreadExecutor();
+
         primaryStage.setTitle("NaSch-Modell");
+
         Canvas canvas = new Canvas(1000, 500);
         WritableImage wi = new WritableImage(1000, 500);
         PixelWriter pw = wi.getPixelWriter()/*canvas.getGraphicsContext2D().getPixelWriter()*/;
-        pw.setColor(5, 5, new Color(1, 0, 0, 1));
+
+
+        this.executor.AddListener(new ISimulationObserver() {
+            private final int lineCount = 500;
+            private int currentLine = 0;
+            private Color vehicleColor = new Color(1, 0, 0, 1);
+            private Color noVehicleColor = new Color(0, 0, 0, 1);
+            private Color nextLineColor = new Color(0, 1, 0, 1);
+
+            @Override
+            public void stepFinished() {
+                System.out.println(currentLine);
+                final int lineToDraw = currentLine;
+                final int lineToClear = getNextLine();
+                nextLine();
+                drawExecutor.submit(() -> {
+                    for (int i = 0; i < 1000; i++) {
+                        pw.setColor(i, lineToDraw, noVehicleColor);
+                    }
+                    simulation.getVehicles().stream().map(Vehicle::getPosition).forEach(position -> pw.setColor(position, lineToDraw, vehicleColor));
+                    for (int i = 0; i < 1000; i++) {
+                        pw.setColor(i, lineToClear, nextLineColor);
+                    }
+                });
+            }
+
+            private int getNextLine() {
+                return (currentLine + 1) % lineCount;
+            }
+
+            private void nextLine() {
+                currentLine++;
+                if (currentLine == lineCount) {
+                    this.currentLine = 0;
+                }
+            }
+        });
         ImageView imageView = new ImageView(wi);
         Group root = new Group();
         root.getChildren().add(canvas);
         root.getChildren().add(imageView);
         primaryStage.setScene(new Scene(root, 1000, 500));
+        executor.start();
         primaryStage.show();
+    }
+
+    @Override
+    public void stop() {
+        System.out.println("ending");
+        executor.stop();
+        drawExecutor.shutdown();
+        System.out.println("ended");
     }
 }
