d28de9a6b7d4da7f78d1f1f558b83af048b9ebd9
Leandro Späth <leandro.spaeth@gmail.com>
2018-03-10 22:37:40 +0100

    [KPLXI] add safe block placing

--- 10_KPLXI_irupu/src/main/Game.java
+++ 10_KPLXI_irupu/src/main/Game.java
@@ -4,32 +4,40 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Game {
-    List<Figure> circles = new ArrayList<>();
-    List<Figure> blocks = new ArrayList<>();
+    private List<Figure> circles = new ArrayList<>();
+    private List<Figure> blocks = new ArrayList<>();
+    int[][] field = new int[Configuration.instance.screenHeight][Configuration.instance.screenWidth];
 
     public Game() {
-        circles.add(new Figure(1, 1));
-        circles.add(new Figure(8, 1));
-        circles.add(new Figure(9, 1));
-        circles.add(new Figure(0, 2));
-        circles.add(new Figure(3, 2));
-        circles.add(new Figure(5, 2));
-        circles.add(new Figure(7, 2));
-        circles.add(new Figure(9, 3));
-        circles.add(new Figure(2, 4));
-        circles.add(new Figure(7, 5));
-        circles.add(new Figure(8, 5));
-        circles.add(new Figure(3, 6));
-        circles.add(new Figure(4, 6));
-        circles.add(new Figure(6, 6));
-        circles.add(new Figure(0, 7));
-        circles.add(new Figure(7, 8));
-        circles.add(new Figure(0, 9));
-        circles.add(new Figure(3, 9));
-        circles.add(new Figure(8, 9));
+        for (int i = 0; i < Configuration.instance.screenHeight; i++) {
+            for (int j = 0; j < Configuration.instance.screenWidth; j++) {
+                field[i][j] = 0;
+            }
+        }
 
-        blocks.add(new Figure(1, 1, false));
-        blocks.add(new Figure(3, 1, true));
+        addCircle(new Figure(1, 1));
+        addCircle(new Figure(8, 1));
+        addCircle(new Figure(9, 1));
+        addCircle(new Figure(0, 2));
+        addCircle(new Figure(3, 2));
+        addCircle(new Figure(5, 2));
+        addCircle(new Figure(7, 2));
+        addCircle(new Figure(9, 3));
+        addCircle(new Figure(2, 4));
+        addCircle(new Figure(7, 5));
+        addCircle(new Figure(8, 5));
+        addCircle(new Figure(3, 6));
+        addCircle(new Figure(4, 6));
+        addCircle(new Figure(6, 6));
+        addCircle(new Figure(0, 7));
+        addCircle(new Figure(7, 8));
+        addCircle(new Figure(0, 9));
+        addCircle(new Figure(3, 9));
+        addCircle(new Figure(8, 9));
+
+        addBlock(new Figure(1, 1, false));
+        addBlock(new Figure(3, 1, true));
+
     }
 
     public List<Figure> getCircles() {
@@ -39,4 +47,56 @@ public class Game {
     public List<Figure> getBlocks() {
         return blocks;
     }
+
+    public void addBlock(Figure block) {
+        if (!checkBlockPlaceable(block))
+            return;
+
+        field[block.getY()][block.getX()] += 10;
+        if (block.getOrientation()) { //vertical
+            field[block.getY()][block.getX() - 1] += 10;
+            field[block.getY()][block.getX() + 1] += 10;
+        } else {
+            field[block.getY() - 1][block.getX()] += 10;
+            field[block.getY() + 1][block.getX()] += 10;
+        }
+
+        blocks.add(block);
+    }
+
+    public void addCircle(Figure circle) {
+        field[circle.getY()][circle.getX()] += 1;
+        circles.add(circle);
+    }
+
+    public boolean checkBlockPlaceable(Figure block) {
+        if (block.getOrientation()) { //vertical
+            if (!(block.getX() >= 0 && block.getX() < Configuration.instance.screenWidth))
+                return false;
+            else if (!(block.getY() >= 1 && block.getY() < Configuration.instance.screenHeight - 1))
+                return false;
+        } else {
+            if (!(block.getX() >= 1 && block.getX() < Configuration.instance.screenWidth - 1))
+                return false;
+            else if (!(block.getY() >= 0 && block.getY() < Configuration.instance.screenHeight))
+                return false;
+        }
+
+
+        int sum = 0;
+        sum += field[block.getY()][block.getX()];
+        if (block.getOrientation()) { //vertical
+            sum += field[block.getY() - 1][block.getX()];
+            sum += field[block.getY() + 1][block.getX()];
+        } else {
+            sum += field[block.getY()][block.getX() - 1];
+            sum += field[block.getY()][block.getX() + 1];
+        }
+        if (sum != 1) { //checks that only on one circle and no other block intersected
+            return false;
+        }
+
+        return true;
+    }
+
 }
